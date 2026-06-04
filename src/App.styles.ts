0cccc06e5836b8a7711fd52730f1a2a4095ef111
Manoel Victor Ferreira <manoel.vict.fer@gmail.com>
2026-06-04 20:37:54 -0300

    Making Playable by making the right stay, adjusting media querry

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -7,7 +7,7 @@ export const Container = styled.div`
   display: flex;
   padding: 50px 0;
 
-  @media (max-width: 750px) {
+  @media (max-width: 790px) {
     flex-direction: column;
   }
 `;
@@ -17,7 +17,7 @@ export const Info = styled.div`
   flex-direction: column;
   width: auto;
 
-  @media (max-width: 750px) {
+  @media (max-width: 790px) {
     margin-bottom: 50px;
     align-items: center;
   }
@@ -31,7 +31,7 @@ export const InfoArea = styled.div`
   width: 100%;
   margin: 12px 0;
 
-  @media (max-width: 750px) {
+  @media (max-width: 790px) {
     display: flex;
     justify-content: space-around;
     text-align: center;
@@ -43,7 +43,7 @@ export const GridArea = styled.div`
   display: flex;
   justify-content: flex-end;
 
-  @media (max-width: 750px) {
+  @media (max-width: 790px) {
     justify-content: center;
     margin: 0 20px;
   }
@@ -54,4 +54,8 @@ export const Grid = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   gap: 12px;
+
+  @media (max-width: 790px) {
+   grid-template-columns: repeat(3, 1fr);
+  }
 `;
